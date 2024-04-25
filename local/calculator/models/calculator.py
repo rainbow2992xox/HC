4fdec2878fece6fbc5baf67506ed889869b7fb20
Admin <rainbowlin2992@gmail.com>
2024-04-25 20:53:53 +0800

    init

--- local/calculator/models/calculator.py
+++ local/calculator/models/calculator.py
@@ -2,8 +2,8 @@ import os
 
 from odoo import models, fields, api
 import math, requests
-import pyecharts.options as opts
-from pyecharts.charts import Line
+# import pyecharts.options as opts
+# from pyecharts.charts import Line
 
 class calculator(models.Model):
     _name = 'hc.calculator'
@@ -289,24 +289,24 @@ class calculator(models.Model):
     @api.depends("pci_referral_incentive_amount", "follow_up_incentive_amount", "ecg_incentive_amount")
     def _compute_chat(self):
 
-
-        x_data = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-        y_data = [820, 932, 901, 934, 1290, 1330, 1320]
-        html = Line().set_global_opts(
-                tooltip_opts=opts.TooltipOpts(is_show=False),
-                xaxis_opts=opts.AxisOpts(type_="category"),
-                yaxis_opts=opts.AxisOpts(
-                    type_="value",
-                    axistick_opts=opts.AxisTickOpts(is_show=True),
-                    splitline_opts=opts.SplitLineOpts(is_show=True),
-                ),
-            ).add_xaxis(xaxis_data=x_data).add_yaxis(
-                series_name="",
-                y_axis=y_data,
-                symbol="emptyCircle",
-                is_symbol_show=True,
-                label_opts=opts.LabelOpts(is_show=False),
-            ).render_embed()
+        #
+        # x_data = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        # y_data = [820, 932, 901, 934, 1290, 1330, 1320]
+        # html = Line().set_global_opts(
+        #         tooltip_opts=opts.TooltipOpts(is_show=False),
+        #         xaxis_opts=opts.AxisOpts(type_="category"),
+        #         yaxis_opts=opts.AxisOpts(
+        #             type_="value",
+        #             axistick_opts=opts.AxisTickOpts(is_show=True),
+        #             splitline_opts=opts.SplitLineOpts(is_show=True),
+        #         ),
+        #     ).add_xaxis(xaxis_data=x_data).add_yaxis(
+        #         series_name="",
+        #         y_axis=y_data,
+        #         symbol="emptyCircle",
+        #         is_symbol_show=True,
+        #         label_opts=opts.LabelOpts(is_show=False),
+        #     ).render_embed()
 
         for rec in self:
             rec.chart = '''<img src="//static/line.png" >'''
